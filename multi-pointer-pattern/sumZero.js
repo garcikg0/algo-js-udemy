39c4124a92c0b0ca15c1314ba11e6d290f8fc905
garcikg0 <kg.gar91@gmail.com>
2021-02-02 14:52:27 -0500

    sumZero bug fix

--- multi-pointer-pattern/sumZero.js
+++ multi-pointer-pattern/sumZero.js
@@ -2,21 +2,22 @@
 //The function should find the first pair where the sum is 0. 
 //Return an array that includes both values that sum to zero or undefined if a pair does not exist. 
 
-function sumZero(arr){
+const arr = [-4,-3,-2,-1,0,1,2,3,10]
+
+function sumZero(arr) {
     let left = 0;
-    let right = arr.length -1;
+    let right = arr.length - 1;
     while (left < right){
         let sum = arr[left] + arr[right];
         if (sum === 0){
+            console.log([arr[left], arr[right]])
             return [arr[left], arr[right]]
         } else if (sum > 0){
-            right --;
+            right--;
         } else {
-            left ++;
+            left++;
         }
     }
 }
 
-const arr = [-4, -3, -2, -1, 0, 1, 2, 3, 10]
-
-sumZero(arr)
+sumZero(arr);
